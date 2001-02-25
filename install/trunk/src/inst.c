8afd9eedc9f32e27a3ef9d74cbb0a35909266a10
jhall1 <jhall1@6ac86273-5f31-0410-b378-82cca8765d1b>
2001-02-25 21:45:53 +0000

    major interface cleanup, some code cleanup: removed getch_yn in favor of select_yn.  Also, reformatted the screen that is presented to the user, especially around boxes.
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@152 6ac86273-5f31-0410-b378-82cca8765d1b

--- install/trunk/src/inst.c
+++ install/trunk/src/inst.c
@@ -1,6 +1,6 @@
 /* Functions to install a disk series, or a package */
 
-/* Copyright (C) 1998,1999,2000,2001 Jim Hall <jhall@freedos.org> */
+/* Copyright (C) 1998-2001 Jim Hall <jhall@freedos.org> */
 
 /*
   This program is free software; you can redistribute it and/or modify
@@ -24,13 +24,14 @@
 
 #include "catgets.h"			/* DOS catopen, catgets */
 #include "bargraph.h"			/* for bargraph() */
+#include "box.h"			/* box() */
 #include "dat.h"			/* data file functions */
-#include "cat.h"			/* for cat() */
-#include "getch_yn.h"			/* for getch() hack */
+#include "cat.h"			/* for cat_file() */
 #include "inst.h"			/* for this file */
 #include "isfile.h"			/* for isfile() */
 #include "lsm.h"			/* Linux LSM files */
 #include "repaint.h"			/* for repaint() */
+#include "sel_list.h"			/* select_yn() */
 #include "unz.h"			/* for UzpMain() */
 
 /* Globals */
@@ -43,10 +44,10 @@ set_install (const char *diskset, char *fromdir, char *destdir)
 {
   /* Variables */
 
-  char endfile[MAXPATH];		/* marks end of series */
-  char descfile[MAXPATH];		/* description file */
-  char datfile[MAXPATH];		/* current DAT file */
-  char ext[MAXEXT];			/* file extension */
+  char endfile[_MAX_PATH];		/* marks end of series */
+  char descfile[_MAX_PATH];		/* description file */
+  char datfile[_MAX_PATH];		/* current DAT file */
+  char ext[_MAX_EXT];			/* file extension */
   char *s;
   int disknum = 0;			/* current disk number */
   int ch;
@@ -55,14 +56,14 @@ set_install (const char *diskset, char *fromdir, char *destdir)
 
   /* Create the filenames */
 
-  fnmerge (endfile, "", fromdir, diskset, "END");
-  fnmerge (descfile, "", fromdir, diskset, "TXT");
+  _makepath (endfile, "", fromdir, diskset, "END");
+  _makepath (descfile, "", fromdir, diskset, "TXT");
 
   /* Print the name of the series we are working on */
 
   repaint_empty();
   s = catgets (cat, 3, 2, "Installing series: ");
-  gotoxy (1, 3);
+  gotoxy (2, 3);
   cputs (s);
   cputs (diskset);
 
@@ -79,26 +80,26 @@ set_install (const char *diskset, char *fromdir, char *destdir)
        the endfile was found. */
 
     sprintf (ext, "%d", ++disknum);
-    fnmerge (datfile, "", fromdir, diskset, ext);
+    _makepath (datfile, "", fromdir, diskset, ext);
 
     if (!isfile (datfile)) {
       /* Does the endfile exist? */
 
       if (isfile (endfile)) {
 	s = catgets (cat, 3, 5, "Done installing this disk series.");
-	gotoxy (1, 10);
+	gotoxy (2, 10);
 	cputs (s);
 
 	s = catgets (cat, 3, 10, "If you are installing other disk series, please insert");
-	gotoxy (1, 15);
+	gotoxy (2, 15);
 	cputs (s);
 
 	s = catgets (cat, 3, 11, "disk #1 of the next series in the drive now.");
-	gotoxy (1, 16);
+	gotoxy (2, 16);
 	cputs (s);
 
 	s = catgets (cat, 1, 0, "Press any key to continue");
-	gotoxy (1, 25);
+	gotoxy (2, 25);
 	cputs (s);
 
 	getch();
@@ -111,27 +112,24 @@ set_install (const char *diskset, char *fromdir, char *destdir)
 
       do {
 	s = catgets (cat, 3, 4, "Can't find data file for this install disk!");
-	gotoxy (1, 10);
+	gotoxy (2, 10);
 	cputs (s);
 
-	gotoxy (1, 11);
+	gotoxy (2, 11);
 	cputs (datfile);
 
 	s = catgets (cat, 3, 8, "You may not have the right install floppy in the drive.");
-	gotoxy (1, 15);
+	gotoxy (2, 15);
 	cputs (s);
 
 	s = catgets (cat, 3, 9, "Double check that you have the right disk and try again.");
-	gotoxy (1, 16);
+	gotoxy (2, 16);
 	cputs (s);
 
 	s = catgets (cat, 2, 3, "Continue installing this disk? [yn]");
-	gotoxy (1, 25);
-	cputs (s);
+	ch = select_yn (s, "Yes", "No");
 
-	ch = getch_yn();
-
-	if ((ch != 'y') && (ch != 'Y'))
+	if (ch)
 	  {
 	    /* user has decided to quit this series */
 	    return (ret);
@@ -152,7 +150,7 @@ inst_t
 disk_install(const char *datfile, const char *descfile,
 	     char *fromdir, char *destdir)
 {
-  char lsmfile[MAXPATH];		/* Linux software map file */
+  char lsmfile[_MAX_PATH];		/* Linux software map file */
   char *s;
 
   int dat_size = 30;			/* malloc size of the dat array */
@@ -178,7 +176,7 @@ disk_install(const char *datfile, const char *descfile,
       fprintf (stderr, "Unable to allocate enough memory for install floppy data file!\n");
 
       s = catgets (cat, 1, 0, "Press any key to continue");
-      gotoxy (1, 25);
+      gotoxy (2, 25);
       cputs (s);
 
       getch();
@@ -192,7 +190,7 @@ disk_install(const char *datfile, const char *descfile,
       fprintf (stderr, "The install floppy data file is empty!\n");
 
       s = catgets (cat, 1, 0, "Press any key to continue");
-      gotoxy (1, 25);
+      gotoxy (2, 25);
       cputs (s);
 
       getch();
@@ -206,12 +204,14 @@ disk_install(const char *datfile, const char *descfile,
     /* Print the screen and progress bargraph */
 
     repaint_empty();
-    gotoxy (15, 20);
+
+    box (14, 17, 66, 19);
+    gotoxy (15, 18);
     bargraph (i, dat_count, 50 /* width */);
 
     /* Print the package name */
 
-    gotoxy (1, 5);
+    gotoxy (2, 5);
     s = catgets (cat, 3, 3, "Package: ");
     cputs (s);
 
@@ -221,28 +221,28 @@ disk_install(const char *datfile, const char *descfile,
 
     /* Generate the lsmfile name */
 
-    fnmerge (lsmfile, "", fromdir, dat_ary[i].name, "LSM");
+    _makepath (lsmfile, "", fromdir, dat_ary[i].name, "LSM");
 
     if (isfile (lsmfile))
       {
-	lsm_description (8, 1, 10, lsmfile);
+	lsm_description (8, 2, 10, lsmfile);
       }
     else
       {
 	/* no lsm file. try it again with a plain txt file */
 
-	fnmerge (lsmfile, "", fromdir, dat_ary[i].name, "TXT");
+	_makepath (lsmfile, "", fromdir, dat_ary[i].name, "TXT");
 
 	if (isfile (lsmfile))
 	  {
-	    gotoxy (1, 8);
+	    gotoxy (2, 8);
 	    cat_file (lsmfile, 10 /* no. lines */);
 	  }
       }
 
     /* Find out which ones the user wants to install */
 
-    gotoxy (1, 23);
+    gotoxy (2, 25);
     switch (dat_ary[i].rank) {
     case 'n':
     case 'N':
@@ -273,13 +273,10 @@ disk_install(const char *datfile, const char *descfile,
 
 	/* Does the user want to continue anyway? */
 
-	gotoxy (1, 25);
 	s = catgets (cat, 2, 3, "Continue installing this disk? [yn]");
-	cputs (s);
+	ch = select_yn(s, "Yes", "No");
 
-	ch = getch_yn();
-
-	if ((ch != 'y') && (ch != 'Y'))
+	if (ch)
 	  {
 	    return (this);
 	  }
@@ -294,13 +291,10 @@ disk_install(const char *datfile, const char *descfile,
 
       /* Ask the user if you want to install it */
 
-      gotoxy (1, 23);
       s = catgets (cat, 2, 4, "Install this package? [yn]");
-      cputs (s);
+      ch = select_yn (s, "Yes", "No");
 
-      ch = getch_yn();
-
-      if ((ch == 'y') || (ch == 'Y'))
+      if (ch)
 	{
 	  ret = unzip_file (dat_ary[i].name, fromdir, destdir);
 
@@ -308,11 +302,11 @@ disk_install(const char *datfile, const char *descfile,
 	    {
 	      /* Print a warning message */
 
-	      gotoxy (1, 23);
+	      gotoxy (2, 23);
 	      s = catgets (cat, 3, 7, "WARNING!  Failed to install OPTIONAL package.");
 	      cputs (s);
 
-	      gotoxy (1, 25);
+	      gotoxy (2, 25);
 	      s = catgets (cat, 1, 0, "Press any key to continue");
 	      cputs (s);
 
