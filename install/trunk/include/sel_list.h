aee32e3b4ae1d5dc7ad51c51f9965e0e0adff207
jhall1 <jhall1@6ac86273-5f31-0410-b378-82cca8765d1b>
2001-02-25 21:44:43 +0000

    updates to include select_yn() prototype
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@151 6ac86273-5f31-0410-b378-82cca8765d1b

--- install/trunk/include/sel_list.h
+++ install/trunk/include/sel_list.h
@@ -1,10 +1,9 @@
-/* $Id$ */
-
 #ifndef _SEL_LIST_H
 #define _SEL_LIST_H
 
 /* Function prototypes */
 
 int select_list (int optc, char *optv[]);
+int select_yn (char *prompt, char *yes, char *no);
 
 #endif /* _SEL_LIST_H */
