596f4d5dd366c0436756e698c488c15672b0650c
(no author) <(no author)@6ac86273-5f31-0410-b378-82cca8765d1b>
2001-08-27 02:08:29 +0000

    This commit was manufactured by cvs2svn to create tag 'VER_3_7_5'.
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@302 6ac86273-5f31-0410-b378-82cca8765d1b

---
+++ install/tags/VER_3_7_5/src/repaint.c
@@ -0,0 +1,41 @@
+/* $Id$ */
+
+/* Copyright (C) 1999,2000 Jim Hall <jhall@freedos.org> */
+
+/*
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+#include <stdio.h>
+#include <conio.h>
+
+#include "box.h"
+#include "globals.h"	/* for cat - catalog id - and #include "catgets.h" */
+#include "text.h"       /* for strings printed */
+
+void
+repaint_empty (void)
+{
+  char *s = catgets (cat, SET_GENERAL, MSG_TITLE, MSG_TITLE_STR);
+
+  /* Clear the screen and repaint an empty screen for the installer */
+  clrscr();
+
+  gotoxy (2, 1);
+  cputs (s);
+
+  box (1, 2, 80, 24);
+}
