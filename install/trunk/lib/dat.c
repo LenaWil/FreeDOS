2174ff129491f21766652f0f10a4e1478e016989
jhall1 <jhall1@6ac86273-5f31-0410-b378-82cca8765d1b>
2001-08-27 01:15:06 +0000

    updates from Jeremy to support dynamic dat file reading/handling.
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@287 6ac86273-5f31-0410-b378-82cca8765d1b

--- install/trunk/lib/dat.c
+++ install/trunk/lib/dat.c
@@ -30,21 +30,55 @@
 #define STRLEN 80				/* size of a string */
 
 
-int
-dat_fread (FILE *stream, dat_t *dat_ary, int dat_size)
+dat_t *
+dat_fread (FILE *stream, int *count)
 {
-  /* reads a dat file from a stream, into an array pointed to by dat_ary,
-     of max size dat_size.  Returns the number of elements in the
-     array (0 may indicate an error.) */
+  /* reads a dat file, into an array pointed to by dat_ary.
+     The number of elements in the array (0 may indicate an 
+     error) is placed in count, returns dat_ary, the array
+     which the user should free() when no longer needed. 
+  */
 
   char str[STRLEN];				/* for reading the line */
   char *s;					/* temporary pointer */
-  int count;					/* counter for ary size */
+  dat_t *dat_ary;				/* array to hold data */
+  int dat_size = 0;			/* holds needed malloc'd size */
+  fpos_t iFilePosition;             /* stores initial file position */
 
   /* Read the file until end of file */
 
-  count = 0;
+  *count = 0;
 
+  /* Do two reads, 1st time through just count how many there are */
+
+  /* 1st get file position, so can reset after 1st time through,
+     we could just seek to the beginning, but there is guarentee
+     the stream is at the beginning.
+  */
+  if (fgetpos(stream, &iFilePosition)) return NULL;
+
+  /* Do 1st pass on the file data, getting just the count */
+  while (fgets (str, STRLEN, stream) != NULL) {
+
+    /* Ignore the line if 1st character is a # */
+    if (*str == '#') continue;
+
+    /* Ignore blank lines */
+    if ( (*str == '\0') || ((*str == '\n') && (*(str+1) == '\0')) ) continue;
+
+    /* Increment counter */
+    dat_size++;
+
+  } /* while */
+
+  /* Reset file position */
+  if (fsetpos(stream, &iFilePosition)) return NULL;
+
+  /* Allocate necessary space (should be all space that is needed) */
+  if ( (dat_size == 0) || ((dat_ary = (dat_t *)malloc(sizeof(dat_t) * dat_size)) == NULL) )
+    return NULL;
+
+  /* Actually read in the data */
   while (fgets (str, STRLEN, stream) != NULL) {
 
     /* Ignore the line if 1st character is a # */
@@ -59,51 +93,52 @@ dat_fread (FILE *stream, dat_t *dat_ary, int dat_size)
 
     if (s == NULL)
       {
-	strncpy (dat_ary[count].name, "", 1);
+	strncpy (dat_ary[*count].name, "", 1);
       }
 
     else
       {
-	strncpy (dat_ary[count].name, s, DAT_NAME_LEN);
+	strncpy (dat_ary[*count].name, s, DAT_NAME_LEN);
       }
 
     s = strtok (NULL, " :");
 
     if (s == NULL)
       {
-	dat_ary[count].rank = '?';
+	dat_ary[*count].rank = '?';
       }
 
     else
       {
-	dat_ary[count].rank = s[0];
+	dat_ary[*count].rank = s[0];
       }
     
     /* Check on the length of the array */
 
-    count++;
+    *count += 1;
 
-    if (count >= dat_size) {
-      /* The array is maxed out - give up. */
-
-      return (dat_size);
+    if (*count > dat_size) {
+        printf("INTERNAL ERROR: 2nd processing of data file yielded extra entries!\n");
+        exit(1);
     }
   } /* while */
 
   /* Return */
 
-  return (count);
+  return (dat_ary);
 }
 
-int
-dat_read (const char *filename, dat_t *dat_ary, int dat_size)
+dat_t *
+dat_read (const char *filename, int *count)
 {
-  /* reads a dat file, into an array pointed to by dat_ary, of max
-     size dat_size.  Returns the number of elements in the array (0
-     may indicate an error.) */
+  /* reads a dat file, into an array pointed to by dat_ary.
+     The number of elements in the array (0 may indicate an 
+     error) is placed in count, returns dat_ary, the array
+     which the user should free() when no longer needed. 
+  */
 
   FILE *stream;
-  int ret;
+  dat_t *ret;
 
   /* Open the file */
 
@@ -116,7 +151,7 @@ dat_read (const char *filename, dat_t *dat_ary, int dat_size)
 
   /* Read the file */
 
-  ret = dat_fread (stream, dat_ary, dat_size);
+  ret = dat_fread (stream, count);
 
   /* Close the file, and quit */
 
