ccce8e73951d065b1186cd4e727faf96262e6ddb
(no author) <(no author)@6ac86273-5f31-0410-b378-82cca8765d1b>
2002-02-09 01:02:11 +0000

    This commit was manufactured by cvs2svn to create tag 'ke2026'.
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@350 6ac86273-5f31-0410-b378-82cca8765d1b

---
+++ kernel/tags/ke2026/drivers/wrpcclk.asm
@@ -0,0 +1,75 @@
+;
+; File:
+;                         wrpcclk.asm
+; Description:
+;                  WritePCClock - sysclock support
+;
+;                       Copyright (c) 1995
+;                       Pasquale J. Villani
+;                       All Rights Reserved
+;
+; This file is part of DOS-C.
+;
+; DOS-C is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version
+; 2, or (at your option) any later version.
+;
+; DOS-C is distributed in the hope that it will be useful, but
+; WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+; the GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public
+; License along with DOS-C; see the file COPYING.  If not,
+; write to the Free Software Foundation, 675 Mass Ave,
+; Cambridge, MA 02139, USA.
+;
+; $Header$
+;
+        %include "..\kernel\segs.inc"
+
+segment HMA_TEXT
+
+
+;
+;       VOID WritePCClock(Ticks)
+;       ULONG Ticks;
+;
+                global  _WritePCClock
+_WritePCClock:
+                mov     bx,sp
+;               Ticks = 4
+                mov     cx,word [ss:bx+4]
+                mov     dx,word [ss:bx+2]      ;Ticks
+                mov     ah,1
+                int     1ah
+                ret
+
+; Log: wrpcclk.asm,v 
+;
+; Revision 1.3  1999/08/10 17:21:08  jprice
+; ror4 2011-01 patch
+;
+; Revision 1.2  1999/03/29 17:08:31  jprice
+; ror4 changes
+;
+; Revision 1.1.1.1  1999/03/29 15:40:35  jprice
+; New version without IPL.SYS
+;
+; Revision 1.2  1999/01/22 04:16:40  jprice
+; Formating
+;
+; Revision 1.1.1.1  1999/01/20 05:51:00  jprice
+; Imported sources
+;
+;
+;   Rev 1.2   29 Aug 1996 13:07:12   patv
+;Bug fixes for v0.91b
+;
+;   Rev 1.1   01 Sep 1995 18:50:42   patv
+;Initial GPL release.
+;
+;   Rev 1.0   02 Jul 1995  8:01:30   patv
+;Initial revision.
+;
---
+++ kernel/tags/ke2026/kernel/misc.c
@@ -0,0 +1,122 @@
+/****************************************************************/
+/*                                                              */
+/*                           misc.c                             */
+/*                                                              */
+/*                 Miscellaneous Kernel Functions               */
+/*                                                              */
+/*                      Copyright (c) 1993                      */
+/*                      Pasquale J. Villani                     */
+/*                      All Rights Reserved                     */
+/*                                                              */
+/* This file is part of DOS-C.                                  */
+/*                                                              */
+/* DOS-C is free software; you can redistribute it and/or       */
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation; either version */
+/* 2, or (at your option) any later version.                    */
+/*                                                              */
+/* DOS-C is distributed in the hope that it will be useful, but */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of   */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See    */
+/* the GNU General Public License for more details.             */
+/*                                                              */
+/* You should have received a copy of the GNU General Public    */
+/* License along with DOS-C; see the file COPYING.  If not,     */
+/* write to the Free Software Foundation, 675 Mass Ave,         */
+/* Cambridge, MA 02139, USA.                                    */
+/****************************************************************/
+
+#include "portab.h"
+
+#ifdef VERSION_STRINGS
+static BYTE *miscRcsId =
+    "$Id$";
+#endif
+
+#include "globals.h"
+#ifndef I86
+
+VOID strcpy(REG BYTE * d, REG BYTE * s)
+{
+  while (*s)
+    *d++ = *s++;
+  *d = '\0';
+}
+
+VOID fstrcpy(REG BYTE FAR * d, REG BYTE FAR * s)
+{
+  while (*s)
+    *d++ = *s++;
+  *d = '\0';
+}
+
+VOID fstrncpy(BYTE FAR * d, BYTE FAR * s, REG COUNT n)
+{
+  while (*s && n--)
+    *d++ = *s++;
+  *d = '\0';
+}
+
+VOID memcpy(REG VOID * d, REG VOID * s, REG COUNT n)
+{
+  while (n--)
+    *d++ = *s++;
+}
+
+VOID fmemcpy(REG VOID FAR * d, REG VOID FAR * s, REG COUNT n)
+{
+  while (n--)
+    *((BYTE FAR *) d)++ = *((BYTE FAR *) s)++;
+}
+
+VOID fmemset(REG VOID FAR * s, REG int ch, REG COUNT n)
+{
+  while (n--)
+    *((BYTE FAR *) s)++ = ch;
+}
+
+#endif
+
+/*
+ * Log: misc.c,v - for newer entries see "cvs log misc.c"
+ *
+ * Revision 1.4  2000/03/09 06:07:11  kernel
+ * 2017f updates by James Tabor
+ *
+ * Revision 1.3  1999/05/03 06:25:45  jprice
+ * Patches from ror4 and many changed of signed to unsigned variables.
+ *
+ * Revision 1.2  1999/04/23 04:24:39  jprice
+ * Memory manager changes made by ska
+ *
+ * Revision 1.1.1.1  1999/03/29 15:42:19  jprice
+ * New version without IPL.SYS
+ *
+ * Revision 1.3  1999/02/01 01:43:28  jprice
+ * Fixed findfirst function to find volume label with Windows long filenames
+ *
+ * Revision 1.2  1999/01/22 04:15:28  jprice
+ * Formating
+ *
+ * Revision 1.1.1.1  1999/01/20 05:51:00  jprice
+ * Imported sources
+ *
+ *
+ *    Rev 1.5   04 Jan 1998 23:14:36   patv
+ * Changed Log for strip utility
+ *
+ *    Rev 1.4   29 May 1996 21:15:18   patv
+ * bug fixes for v0.91a
+ *
+ *    Rev 1.3   19 Feb 1996  3:20:12   patv
+ * Added NLS, int2f and config.sys processing
+ *
+ *    Rev 1.2   01 Sep 1995 17:48:46   patv
+ * First GPL release.
+ *
+ *    Rev 1.1   30 Jul 1995 20:50:28   patv
+ * Eliminated version strings in ipl
+ *
+ *    Rev 1.0   02 Jul 1995  8:06:28   patv
+ * Initial revision.
+ */
