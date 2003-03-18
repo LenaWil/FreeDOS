43ffa7b4a7cd268d9c8cf7f24eb304ca167756e7
bartoldeman <bartoldeman@6ac86273-5f31-0410-b378-82cca8765d1b>
2003-03-18 02:04:18 +0000

    Declare DGROUP
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@559 6ac86273-5f31-0410-b378-82cca8765d1b

--- mem/trunk/source/mem/memsupt.asm
+++ mem/trunk/source/mem/memsupt.asm
@@ -1,51 +1,11 @@
 ;
 ; assembly support routines for FreeDOS MEM
 ;
+        group   DGROUP
+
+        extern  _xms_drv:wrt DGROUP
+
         segment _TEXT class=CODE
-
-; int cdecl is_386_(void)        
-global _is_386_
-_is_386_:
-        pushf
-        pushf
-        pop     ax
-        or      ax, 0x7000
-        push    ax
-        popf
-        pushf
-        pop     ax
-        popf
-        ret
-
-; ulong check_e820(struct e820map far *e820map, ulong counter)
-global _check_e820
-_check_e820:
-        mov	bx, sp
-	push	di
-	push	si
-	les	di, [bx+2]
-	mov	ax, [bx+6]
-	mov	bx, [bx+8]
-	shl	ebx, 0x10
-	mov	bx, ax
-	mov	eax, 0xe820
-	mov	edx, 0x534d4150 ; SMAP
-	mov	ecx, 0x14
-	int	0x15
-	jc	no386
-	cmp	eax, 0x534d4150 ; SMAP
-	je	yes386
- no386:
-	xor	ebx, ebx
- yes386:
-	mov	ax, bx
-	shr	ebx, 16
-	mov	dx, bx
-	pop	si
-	pop	di
-	ret
-
-extern _xms_drv
 
 ; struct xms_dx call_xms_driver_dx_bl_al(unsigned char ah, ushort dx)
 global _call_xms_driver_dx_bl_al
@@ -92,3 +52,45 @@ _call_xms_driver_eax:
 	call	far [_xms_drv]
 	mov	edx, eax
         jmp	short xms_ext_common
+
+; int cdecl is_386_(void)        
+global _is_386_
+_is_386_:
+        pushf
+        pushf
+        pop     ax
+        or      ax, 0x7000
+        push    ax
+        popf
+        pushf
+        pop     ax
+        popf
+        ret
+
+; ulong check_e820(struct e820map far *e820map, ulong counter)
+global _check_e820
+_check_e820:
+        mov	bx, sp
+	push	di
+	push	si
+	les	di, [bx+2]
+	mov	ax, [bx+6]
+	mov	bx, [bx+8]
+	shl	ebx, 0x10
+	mov	bx, ax
+	mov	eax, 0xe820
+	mov	edx, 0x534d4150 ; SMAP
+	mov	ecx, 0x14
+	int	0x15
+	jc	no386
+	cmp	eax, 0x534d4150 ; SMAP
+	je	yes386
+ no386:
+	xor	ebx, ebx
+ yes386:
+	mov	ax, bx
+	shr	ebx, 16
+	mov	dx, bx
+	pop	si
+	pop	di
+	ret
