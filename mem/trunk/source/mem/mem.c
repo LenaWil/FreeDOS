460f31816c6208cba3564f0d791e905398f3956c
bartoldeman <bartoldeman@6ac86273-5f31-0410-b378-82cca8765d1b>
2004-09-19 10:07:07 +0000

    Replace getenv() by our own (~1700 bytes uncompressed).
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@1046 6ac86273-5f31-0410-b378-82cca8765d1b

--- mem/trunk/source/mem/mem.c
+++ mem/trunk/source/mem/mem.c
@@ -647,6 +647,31 @@ static ulong check_e801(void)
 
 #endif
 
+#ifdef __WATCOMC__
+/* WATCOM's getenv is case-insensitive which wastes a lot of space
+   for our purposes. So here's a simple case-sensitive one */
+char *getenv(const char *name)
+{
+  char **envp, *ep;
+  const char *np;
+  char ec, nc;
+
+  for (envp = environ; (ep = *envp) != NULL; envp++) {
+    np = name;
+    do {
+      ec = *ep++;
+      nc = *np++;
+      if (nc == 0) {
+        if (ec == '=')
+          return ep;
+        break;
+      }
+    } while (ec == nc);
+  }
+  return NULL;
+}
+#endif
+
 static char malloc_buffer[30000];
 static size_t mbuffer_idx;
 
