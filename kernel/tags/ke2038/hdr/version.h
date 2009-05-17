50918ab3d64ed4e3492715d0aa813c3f7b56c27c
perditionc <perditionc@6ac86273-5f31-0410-b378-82cca8765d1b>
2009-05-17 01:22:58 +0000

    Kernel 2038 release
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@1384 6ac86273-5f31-0410-b378-82cca8765d1b

---
+++ kernel/tags/ke2038/hdr/version.h
@@ -0,0 +1,50 @@
+/****************************************************************/
+/*                                                              */
+/*                          version.h                           */
+/*                                                              */
+/*                  Common version information                  */
+/*                                                              */
+/*                      Copyright (c) 1997                      */
+/*                      Pasquale J. Villani                     */
+/*                      All Rights Reserved                     */
+/*                                                              */
+/* This file is part of DOS-C.                                  */
+/*                                                              */
+/* DOS-C is free software; you can redistribute it and/or       */
+/* modify it under the terms of the GNU General Public License  */
+/* as published by the Free Software Foundation; either version */
+/* 2, or (at your option) any later version.                    */
+/*                                                              */
+/* DOS-C is distributed in the hope that it will be useful, but */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of   */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See    */
+/* the GNU General Public License for more details.             */
+/*                                                              */
+/* You should have received a copy of the GNU General Public    */
+/* License along with DOS-C; see the file COPYING.  If not,     */
+/* write to the Free Software Foundation, 675 Mass Ave,         */
+/* Cambridge, MA 02139, USA.                                    */
+/****************************************************************/
+
+#ifdef MAIN
+#ifdef VERSION_STRINGS
+static BYTE *date_hRcsId =
+    "$Id$";
+#endif
+#endif
+
+/* This Kernel is at a min Dos Ver. 5.00 or 7.10 with FAT32 support */
+#ifdef WITHFAT32
+#define MAJOR_RELEASE   7
+#define MINOR_RELEASE   10
+#else
+#define MAJOR_RELEASE   6
+#define MINOR_RELEASE   22
+#endif
+
+#define REVISION_SEQ    38      /* returned in BL by int 21 function 30 */
+#define OEM_ID          0xfd    /* FreeDOS, returned in BH by int 21 30 */
+
+#define SOURCE_DATE_STRING "May 16, 2009" /* DATE OF THE NEWEST SOURCE FILE */
+#define KERNEL_BUILD_STRING "2038"   /* used in os_release string */
+
