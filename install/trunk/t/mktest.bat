161cd43b1eb5587ee1ae673fa8ab6a570705899c
jhall1 <jhall1@6ac86273-5f31-0410-b378-82cca8765d1b>
2011-01-01 03:36:47 +0000

    Updated mktest to use the dummy hello.zip file, instead of creating a non-working zip file package in each directory.
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@1542 6ac86273-5f31-0410-b378-82cca8765d1b

--- install/trunk/t/mktest.bat
+++ install/trunk/t/mktest.bat
@@ -3,7 +3,7 @@
 echo MKTEST
 echo Generates *.DIR directories and *empty* *.ZIP files, to test the
 echo new Install program. To undo, delete the *.DIR directories and
-echo contents. NOTE THAT EXISTING *.ZIP FILES MAY BE OVERWRITTEN!
+echo contents. NOTE THAT ANY EXISTING *.ZIP FILES MAY BE OVERWRITTEN!
 
 choice Make test directories now?
 if ERRORLEVEL 2 goto :done
@@ -13,35 +13,35 @@ echo Working ...
 for %d in (base boot devel edit gui net sound util) do mkdir %d.dir
 
 cd base.dir
-for %f in (append assign attrib chkdsk choice command comp country cpidos) do echo %f > %f.zip
+for %f in (append assign attrib chkdsk choice command comp country cpidos) do copy ..\hello.zip %f.zip
 cd ..
 
 cd boot.dir
-for %f in (blackout bootmgr bootspl grub4dos smbtmgr xosl) do echo %f > %f.zip
+for %f in (blackout bootmgr bootspl grub4dos smbtmgr xosl) do copy ..\hello.zip %f.zip
 cd ..
 
 cd devel.dir
-for %f in (arrow basec bin2c bwbasic cc386 clib dflat dmake fasm freebas) do echo %f > %f.zip
+for %f in (arrow basec bin2c bwbasic cc386 clib dflat dmake fasm freebas) do copy ..\hello.zip %f.zip
 cd ..
 
 cd edit.dir
-for %f in (blocek fed freemacs mined ospedit setedit tde vim) do echo %f > %f.zip
+for %f in (blocek fed freemacs mined ospedit setedit tde vim) do copy ..\hello.zip %f.zip
 cd ..
 
 cd gui.dir
-echo opengem > opengem.zip
+copy ..\hello.zip opengem.zip
 cd ..
 
 cd net.dir
-for %f in (arachne code ertos fdsmtpop freexp leetirc lsicq lsppp lynx mime64) do echo %f > %f.zip
+for %f in (arachne code ertos fdsmtpop freexp leetirc lsicq lsppp lynx mime64) do copy ..\hello.zip %f.zip
 cd ..
 
 cd sound.dir
-for %f in (abcmidi drtrack id3tools lame midiplay mpxplay normaliz opencub) do echo %f > %f.zip
+for %f in (abcmidi drtrack id3tools lame midiplay mpxplay normaliz opencub) do copy ..\hello.zip %f.zip
 cd ..
 
 cd util.dir
-for %f in (4dos 7-zip arj bzip2 cabextr cal cdrcache c-lock cmos compinfo) do echo %f > %f.zip
+for %f in (4dos 7-zip arj bzip2 cabextr cal cdrcache c-lock cmos compinfo) do copy ..\hello.zip %f.zip
 cd ..
 
 echo Done!
