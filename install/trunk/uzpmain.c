4c2ad50a5531b89993abc8488d16d6dc94ea0650
jhall1 <jhall1@6ac86273-5f31-0410-b378-82cca8765d1b>
2011-07-07 22:15:04 +0000

    wrapper program for Unzip - using Info-Zip's UzpMain API. also sets the TZ environment variable (if not already set.) **this is kind of a hack, but the Install program barfs if it calls UzpMain() repeatedly. works fine if it calls an external UzpMain.exe. So we'll do this for now, and see if can fix the UzpMain() call later. Maybe someone can help me?
    git-svn-id: http://svn.code.sf.net/p/freedos/svn@1656 6ac86273-5f31-0410-b378-82cca8765d1b

---
+++ install/trunk/uzpmain.c
@@ -0,0 +1,30 @@
+/* uzpmain.c */
+
+/* fake Unzip - using Info-Zip's UzpMain API */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <env.h>
+
+int UzpMain ( int argc, char **argv );		/* from Info-Zip's Unzip */
+
+int
+main (int argc, char **argv)
+{
+  int ret;
+
+  /* wrapper to UzpMain */
+
+  /* set TZ env variable, if not already set */
+
+  if (getenv("TZ") == NULL)
+    {
+      setenv ("TZ", "PST8PDT", 1);		/* env.h */
+    }
+
+  /* printf ("DEBUG: calling UzpMain() . . .\n"); */
+
+  ret = UzpMain (argc, argv);
+  exit (ret);
+}
